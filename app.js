948263ae411d20a05d4ab63c6b24268b3788269f
sardar <97310586+sardar-dev@users.noreply.github.com>
2026-09-10 16:08:06 +0500

    Implement localStorage for list persistence
    Refactor app.js to use localStorage for persistent list management. Added checkbox inputs for each list item.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+
 let input = document.querySelector(".input");
 let ul = document.querySelector(".ul");
 let total = document.querySelector(".total");
 let empty = document.querySelector(".empty");
-let list = [];
+let list = JSON.parse(localStorage.getItem("list")) || [];
+
+show()
 
 function mainn(event) {
   event.preventDefault();
@@ -13,6 +16,7 @@ function mainn(event) {
     return;
   }
   list.push(input.value.trim());
+  localStorage.setItem("list", JSON.stringify(list));
   empty.innerHTML = "";
   input.value = "";
   show();
@@ -21,7 +25,7 @@ function show() {
   ul.innerHTML = "";
 
   for (let i = 0; i < list.length; i++) {
-    ul.innerHTML += ` <li> ${list[i]} <button onclick = "remove(${i})"> ❌ </button> </li>  `;
+    ul.innerHTML += ` <li> <input type="checkbox"> ${list[i]}  <button onclick = "remove(${i})"> ❌ </button> </li>  `;
   }
 
   count = list.length;
@@ -29,10 +33,12 @@ function show() {
 }
 function remove(index) {
   list.splice(index, 1);
+
+  localStorage.setItem("list", JSON.stringify(list));
   show();
 }
 function deleteAll() {
   list = [];
-
+  localStorage.setItem("list", JSON.stringify(list));
   show();
 }
